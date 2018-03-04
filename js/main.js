ce8a1e7c821acf7912e36f83c22539de395cca1a
AdRiAnIlloO <adrigibal@gmail.com>
2018-03-05 00:42:18 +0100

    Adjust player block positions on minigame

--- js/main.js
+++ js/main.js
@@ -2,23 +2,36 @@
 "use strict";
 
 // This script assumes UI toolkit dependencies are already loaded into the page
+const X_DIM = 0;
+const Y_DIM = 1;
+var $g_pongJQuery = null;
+
+// X and Y size ratios from auth QR canvas to game playable area
+var g_playBoundsRatios = Array(2);
+
 $(function () {
-	var $g_pongIFrame = $('#pong-game-html-wrapper').contents();
+	// Pong game iframe is not fully load yet. Wait for it.
+	$('#pong-game-html-wrapper').on('load', function () {
+		$g_pongJQuery = $(this)[0].contentWindow;
+		g_playBoundsRatios[X_DIM] = ($g_pongJQuery.$('#video_camara').width() / $('#qr-canvas').width());
+		g_playBoundsRatios[Y_DIM] = ($g_pongJQuery.$('#video_camara').height() / $('#qr-canvas').height());
+	});
 
 	$('#qr-auth-html-wrapper').load('qr-auth.html', function () {
 		function onQRUserDetected_Playing(event, result) {
-			// Place controlled QR code according to scanned one's origin
-			$g_pongIFrame.find('body').trigger('externalMove',
-				[result.points[1].x, result.points[1].y]);
+			if ($g_pongJQuery != null) {
+				// Calculate and place controlled QR code according to scanned one's origin,
+				// and player block with from minigame
+				var x = ((result.points[0].x + result.points[1].x) * g_playBoundsRatios[X_DIM] / 2);
+				var y = ((result.points[0].y + result.points[1].y) * g_playBoundsRatios[Y_DIM] / 2);
+				$g_pongJQuery.$('body').trigger('externalMove', [x, y]);
+			}
 		}
 
 		// Callback when QR auth + welcome screens are completed by the user
 		function onQRStepsCompleted(event, qrImgURL) {
 			// Fill player block with user QR image
-			$g_pongIFrame.find('#bloque_jugador').html('<img src="' + qrImgURL + '" />');
-
-			// HACK
-			$g_pongIFrame.find('#video_camara').prop('hidden', true);
+			$g_pongJQuery.$('#bloque_jugador').html('<img src="' + qrImgURL + '" />');
 
 			$(document).on('qrUserDetected', onQRUserDetected_Playing);
 		}
